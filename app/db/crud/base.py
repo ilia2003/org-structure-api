21e0ded47b9db046d56e1b105229c274dd1ccc6b
ilia <fedorenkoilia@gmail.com>
2026-03-11 14:29:11 +0100

    fix: Write one base CRUD for departments and employee. Basic method will be enough: get_one get_all create update delete

--- app/db/crud/base.py
+++ app/db/crud/base.py
@@ -18,7 +18,7 @@ class BaseCRUD(Generic[ModelT]):
         result = await self.session.execute(query)
         return result.scalar_one_or_none()
 
-    async def get_many(self, **filters: Any) -> list[ModelT]:
+    async def get_all(self, **filters: Any) -> list[ModelT]:
         query = sa.select(self.model).filter_by(**filters)
         result = await self.session.execute(query)
         return list(result.scalars().all())
@@ -30,18 +30,14 @@ class BaseCRUD(Generic[ModelT]):
         await self.session.refresh(instance)
         return instance
 
+    async def update(self, instance: ModelT, **values: Any) -> ModelT:
+        for field, value in values.items():
+            setattr(instance, field, value)
+
+        await self.session.flush()
+        await self.session.refresh(instance)
+        return instance
+
     async def delete(self, instance: ModelT) -> None:
         await self.session.delete(instance)
         await self.session.flush()
-
-    @staticmethod
-    def set_offset_limit(
-        query: sa.Select,
-        offset: int | None = None,
-        limit: int | None = None,
-    ) -> sa.Select:
-        if offset is not None:
-            query = query.offset(offset)
-        if limit is not None:
-            query = query.limit(limit)
-        return query
--- app/db/crud/employees.py
+++ app/db/crud/employees.py
@@ -1,28 +1,6 @@
-from uuid import UUID
-
-import sqlalchemy as sa
-
 from app.db.crud.base import BaseCRUD
 from app.db.models.employees import Employee
 
 
 class EmployeeCRUD(BaseCRUD[Employee]):
     model = Employee
-
-    async def list_by_department_id(self, department_id: UUID) -> list[Employee]:
-        query = (
-            sa.select(self.model)
-            .where(self.model.department_id == department_id)
-            .order_by(self.model.full_name.asc())
-        )
-        result = await self.session.execute(query)
-        return list(result.scalars().all())
-
-    async def reassign_department(self, from_department_id: UUID, to_department_id: UUID) -> None:
-        query = (
-            sa.update(self.model)
-            .where(self.model.department_id == from_department_id)
-            .values(department_id=to_department_id)
-        )
-        await self.session.execute(query)
-        await self.session.flush()
